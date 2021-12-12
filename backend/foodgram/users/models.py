89d5cd20a83e1bc979f97ef7227efd752fc9b333
Tony Hat <elkobrat@pm.me>
2021-12-12 17:06:10 +1000

    redefine user model

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+    CHOICES = [
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+    ]
+    first_name = models.CharField(
+        'first_name',
+        max_length=150, )
+
+    last_name = models.CharField(
+        'last_name',
+        max_length=150, )
+
+    username = models.CharField(
+        'username',
+        unique=True,
+        max_length=150)
+
+    email = models.EmailField(
+        'email',
+        unique=True,
+        max_length=254
+    )
+
+    role = models.CharField(
+        'role',
+        max_length=len(max((role_pair[1] for role_pair in CHOICES), key=len)),
+        choices=CHOICES,
+        default=USER
+    )
+
+    def __str__(self):
+        return self.username
+
+    REQUIRED_FIELDS = ['email', 'last_name', 'first_name']
+
+    @property
+    def is_admin(self):
+        return (
+            self.is_superuser
+            or self.is_staff
+            or self.role == User.ADMIN)
+
+    class Meta:
+        ordering = ['username']
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
