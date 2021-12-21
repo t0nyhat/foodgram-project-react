969ad413337063564928e86367e8d11d0ac25cac
Tony Hat <elkobrat@pm.me>
2021-12-21 17:25:04 +1000

    user paginator

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import get_user_model
 from djoser.views import UserViewSet
 
+from .paginator import Paginator
 from .serializers import UserSerializer
 
 User = get_user_model()
 
 
 class UserViewSet(UserViewSet):
+    pagination_class = Paginator
     queryset = User.objects.all()
     serializer_class = UserSerializer
