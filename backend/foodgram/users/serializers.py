cd9f3e53130c6a4fff8e6ac0d4353e61cec3e3c1
Tony Hat <elkobrat@pm.me>
2021-12-21 21:57:32 +1000

    some work with serializer

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,15 +1,25 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from .models import Follow
 
 User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
+
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ("id", "email", "username",
                   "first_name", "last_name", "password")
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError(
+                'Данный username не разрешен, выберите другой')
+        return value
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -26,3 +36,29 @@ class UserSerializer(serializers.ModelSerializer):
         if user.is_anonymous:
             return False
         return False
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    follower = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault())
+    author = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('author', 'follower'),
+                message='Вы уже подписаны на этого пользователя'
+            )
+        ]
+
+    def validate_following(self, user):
+        if user == self.context['request'].user:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя')
+        return user
