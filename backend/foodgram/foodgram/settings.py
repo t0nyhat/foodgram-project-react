723e656993e15a1a17830f8d71ce1ded824e55ab
Tony Hat <elkobrat@pm.me>
2021-12-21 17:39:57 +1000

    djoser permissions

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -139,7 +139,6 @@ DJOSER = {
         'user': 'users.serializers.UserSerializer',
     },
     "PERMISSIONS": {
-        'user': ['rest_framework.permissions.AllowAny'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
     "HIDE_USERS": False,
