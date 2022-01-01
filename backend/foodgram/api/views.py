6058eafa94d3df0da60d7deb6669ace159acd5ef
Tony Hat <elkobrat@pm.me>
2022-01-01 14:52:17 +1000

    IngredientsViewSet added

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
-from .models import Tag
-from .serializers import TagSerializer
+from .filters import IngredientNameFilter
+from .models import Ingredient, Tag
+from .serializers import IngredientSerializer, TagSerializer
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -10,3 +11,11 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TagSerializer
     pagination_class = None
     permission_classes = (AllowAny,)
+
+
+class IngredientsViewSet(viewsets.ModelViewSet):
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
+    pagination_class = None
+    permission_classes = (AllowAny,)
+    filterset_class = IngredientNameFilter
