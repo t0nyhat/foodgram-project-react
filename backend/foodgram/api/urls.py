eaa6db817c2d13afa6236d6d4e15eeebe0d61895
Tony Hat <elkobrat@pm.me>
2022-01-01 14:51:48 +1000

    Ingredient router added

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet
+from .views import IngredientsViewSet, TagViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientsViewSet, basename='ingredients')
 urlpatterns = [
     path('', include(router.urls)),
 ]
