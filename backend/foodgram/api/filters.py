ad7974359a4df86ceebf8cd41476ea4d77585d5b
Tony Hat <elkobrat@pm.me>
2022-01-01 14:53:43 +1000

    filters file added

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,11 @@
+import django_filters as filters
+
+from .models import Ingredient
+
+
+class IngredientNameFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
