51cf2aa9699dce1f4f1eb4bb9671788c8892fc24
Tony Hat <elkobrat@pm.me>
2022-01-04 20:38:48 +1000

    IngredientAmountSerializer and ReceipeSerializer added

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,6 +1,9 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+from users.serializers import UserSerializer
 
-from .models import Ingredient, Tag
+from .models import Ingredient, IngredientAmount, Recipe, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -14,3 +17,66 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = '__all__'
+
+
+class IngredientAmountSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = IngredientAmount
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=IngredientAmount.objects.all(),
+                fields=['ingredient', 'recipe']
+            )
+        ]
+
+
+class ReceipeSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+    author = UserSerializer(read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    ingredients = IngredientAmountSerializer(
+        many=True, read_only=True, source='ingredientamount_set',
+    )
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'tags', 'author', 'name', 'text',
+                  'image', 'ingredients', 'cooking_time',
+                  'is_favorited', 'is_in_shopping_cart')
+
+    def get_is_favorited(self, obj):
+        return True
+
+    def get_is_in_shopping_cart(self, obj):
+        return True
+
+    def validate(self, data):
+        ingredients = self.initial_data.get('ingredients')
+        ingredients = {x['id']: x for x in ingredients}.values()
+        data['ingredients'] = ingredients
+        return data
+
+    def fill_receipt_ingredients(self, ingredients, recipe):
+        for ingredient in ingredients:
+            IngredientAmount.objects.create(
+                recipe=recipe,
+                ingredient_id=ingredient.get('id'),
+                amount=ingredient.get('amount'),
+            )
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        tags_data = self.initial_data.get('tags')
+        recipe.tags.set(tags_data)
+        self.fill_receipt_ingredients(ingredients_data, recipe)
+        return recipe
