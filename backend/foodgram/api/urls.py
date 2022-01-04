b89c0609b4102f062139f4db8cb82c659451765f
Tony Hat <elkobrat@pm.me>
2022-01-04 20:39:28 +1000

    recipes router added

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientsViewSet, TagViewSet
+from .views import IngredientViewSet, ReceipeViewSet, TagViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('tags', TagViewSet, basename='tags')
-router.register('ingredients', IngredientsViewSet, basename='ingredients')
+router.register('recipes', ReceipeViewSet, basename='recipes')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 urlpatterns = [
     path('', include(router.urls)),
 ]
