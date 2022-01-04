0f75b15563aa232ac1464e28c7b48c45cf973db7
Tony Hat <elkobrat@pm.me>
2022-01-04 20:40:21 +1000

    IngredientViewSet and ReceipeViewSet added

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -2,8 +2,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 
 from .filters import IngredientNameFilter
-from .models import Ingredient, Tag
-from .serializers import IngredientSerializer, TagSerializer
+from .models import Ingredient, Recipe, Tag
+from .serializers import IngredientSerializer, ReceipeSerializer, TagSerializer
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
@@ -13,9 +13,18 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (AllowAny,)
 
 
-class IngredientsViewSet(viewsets.ModelViewSet):
+class IngredientViewSet(viewsets.ModelViewSet):
     serializer_class = IngredientSerializer
     queryset = Ingredient.objects.all()
     pagination_class = None
     permission_classes = (AllowAny,)
     filterset_class = IngredientNameFilter
+
+
+class ReceipeViewSet(viewsets.ModelViewSet):
+    serializer_class = ReceipeSerializer
+    queryset = Recipe.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
