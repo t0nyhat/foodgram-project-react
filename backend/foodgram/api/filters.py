2e313aa579e003a2bd3230ffaa25a089c5a9ce9d
Tony Hat <elkobrat@pm.me>
2022-01-07 14:45:34 +1000

    receipe filters added

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,6 +1,9 @@
 import django_filters as filters
+from django.contrib.auth import get_user_model
 
-from .models import Ingredient
+from .models import Ingredient, Recipe
+
+User = get_user_model()
 
 
 class IngredientNameFilter(filters.FilterSet):
@@ -9,3 +12,33 @@ class IngredientNameFilter(filters.FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.AllValuesMultipleFilter(
+        field_name='tags__slug'
+    )
+    is_favorited = filters.BooleanFilter(
+        method='get_is_favorited'
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_is_in_shopping_cart'
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+
+    def get_is_favorited(self, queryset, name, value):
+        user = self.request.user
+        print(value)
+
+        if value:
+            return queryset.filter(favorites__user=user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value:
+            return queryset.filter(cart__user=user)
+        return queryset
